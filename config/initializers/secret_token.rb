0d96c86b6b20baae76632768eb870b51be88439b
Jonathan Ofir <jonofir@gmail.com>
2013-03-08 15:23:03 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alcohoot::Application.config.secret_token = 'abd8f1100af49673e831166a7fb974d8e16e3315ed8a550103c3df8faf5b6df75401d54e5c3dd868ca0952dda265c281f7480cb4f3415ac3bc55683b92cef749'
